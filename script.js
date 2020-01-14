eac815d2befa38491b27ca47b7924a84615a6d50
Anirban Sinha <asinha@inmbp015.local>
2020-01-15 03:46:34 +0530

    Clock automatically running

--- script.js
+++ script.js
@@ -2,18 +2,22 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+var date = new Date();
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+console.log(date);
+console.log(hour+" "+minutes+" "+seconds);
+
+let secondpos =seconds*(360/60);
+let minutepos =minutes*(360/60)+secondpos/60;
+let hourpos =hours*(360/12) + minutepos/12;
+
+
 setInterval(() => {
-  var date = new Date();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
-  console.log(date);
-  console.log(hour+" "+minutes+" "+seconds);
-
-  let secondpos =seconds*(360/60);
-  let minutepos =minutes*(360/60)+secondpos/60;
-  let hourpos =hours*(360/12) + minutepos/12;
-
+  secondpos = secondpos + 6; // (360/60);
+  minutepos = minutepos + (.1); //(1/60)*6;
+  hourpos = hourpos + (1/120); //(30/3600)
 
   HOURHAND.style.transform = "rotate("+hourpos+"deg)";
   MINUTEHAND.style.transform = "rotate("+minutepos+"deg)";
