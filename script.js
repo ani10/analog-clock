ece4889c46a8c04ce08d1d1778b7bbc1e6cf4be5
Anirban Sinha <asinha@inmbp015.local>
2020-01-15 03:25:19 +0530

    clock readrunning

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+setInterval(() => {
+  var date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  console.log(date);
+  console.log(hour+" "+minutes+" "+seconds);
+
+  let secondpos =seconds*(360/60);
+  let minutepos =minutes*(360/60)+secondpos/60;
+  let hourpos =hours*(360/12) + minutepos/12;
+
+
+  HOURHAND.style.transform = "rotate("+hourpos+"deg)";
+  MINUTEHAND.style.transform = "rotate("+minutepos+"deg)";
+  SECONDHAND.style.transform = "rotate("+secondpos+"deg)";
+}, 1000);
