7f3d77954bc35c6eedd960f41e13750c9d5986d3
ani10 <ani.expo10@gmail.com>
2020-02-04 01:18:47 +0530

    bar added

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+const FACE = document.querySelector("#face");
+const HOURMARKS = document.querySelector("#hour-marks");
+const MIDCIRCLE = document.querySelector("#mid-circle");
 
 var date = new Date();
 let hours = date.getHours();
@@ -9,10 +12,9 @@ let seconds = date.getSeconds();
 console.log(date);
 console.log(hour+" "+minutes+" "+seconds);
 
-let secondpos =seconds*(360/60);
-let minutepos =minutes*(360/60)+secondpos/60;
-let hourpos =hours*(360/12) + minutepos/12;
-
+let secondpos = seconds*(360/60);
+let minutepos = minutes*(360/60)+secondpos/60;
+let hourpos = hours*(360/12) + minutepos/12;
 
 setInterval(() => {
   secondpos = secondpos + 6; // (360/60);
